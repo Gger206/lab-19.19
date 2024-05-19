2141db6519174105643d18c72dd477a98119fda7
Gger206 <arteml22012006@gmail.com>
2024-05-19 21:21:49 +0300

    task 3

--- library.c
+++ library.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include "lib\data_structures\String_lib.h"
 #include "lib\data_structures\Void_Vector_lib.h"
+#include  <stdbool.h>
 
 void generate_random_matrix_file(const char* filename, size_t n) {
 
@@ -311,8 +312,156 @@ void test_convert_float() {
     test_convert_float_3_three_numbers();
 }
 
+void generate_expression(const char *file_name) {
+
+    FILE *file = fopen(file_name, "w");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    int x1 = rand() % 10;
+    int x2 = rand() % 10;
+    int x3 = rand() % 10;
+
+    char operators[] = "+-*/";
+    char op1 = operators[rand() % 4];
+    char op2 = operators[rand() % 4];
+
+    bool one_operation = rand() % 2;
+
+    if (one_operation)
+        fprintf(file, "(%d %c %d)", x1, op1, x2);
+    else
+        fprintf(file, "(%d %c %d) %c %d", x1, op1, x2, op2, x3);
+
+    fclose(file);
+}
+
+void calculate_expression(const char* filename) {
+    FILE* file = fopen(filename, "r+");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    int x1, x2, x3;
+    char op1, op2;
+    char open_bracket, close_bracket;
+    float result;
+
+    int amount_element = fscanf(file, "%c%d %c %d%c %c %d", &open_bracket, &x1, &op1, &x2, &close_bracket, &op2, &x3);
+
+    bool two_operation = amount_element == 7 ? true : false;
+
+    switch (op1) {
+        case '+':
+            result = x1 + x2;
+            break;
+        case '-':
+            result = x1 -x2;
+            break;
+        case '*':
+            result = x1 * x2;
+            break;
+        case '/':
+            if (x2 == 0) {
+                fprintf(stderr, "Zero division");
+                exit(1);
+            }
+            result = x1 / x2;
+            break;
+        default:
+            fprintf(stderr,"unknown operation");
+            exit(1);
+    }
+
+    if (two_operation) {
+        switch (op2) {
+            case '+':
+                result += x3;
+                break;
+            case '-':
+                result -= x3;
+                break;
+            case '*':
+                result *= x3;
+                break;
+            case '/':
+                if (x3 == 0) {
+                    fprintf(stderr, "Zero division");
+                    exit(1);
+                }
+                result /= x3;
+                break;
+            default:
+                fprintf(stderr,"unknown operation");
+                exit(1);
+        }
+    }
+
+    fseek(file, 0, SEEK_END);
+    fprintf(file, " = %.2f", result);
+
+    fclose(file);
+}
+
+void test_calculate_expression_2_operand() {
+    const char filename[] = "D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_3_test_1.txt";
+
+    char expression[] = "(2 * 4)";
+    FILE* file = fopen(filename, "w");
+
+    fputs(expression, file);
+
+    fclose(file);
+
+    calculate_expression("D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_3_test_1.txt");
+
+    file = fopen(filename, "r");
+
+    char data[100] = "";
+    fgets(data, sizeof(data), file);
+
+    fclose(file);
+
+    char res[] = "(2 * 4) = 8.00 ";
+
+    assert(strcmp(data, res));
+}
+
+void test_calculate_expression_3_operand() {
+    const char filename[] = "D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_3_test_2.txt";
+
+    char expression[] = "(2 * 8) + 3";
+    FILE* file = fopen(filename, "w");
+
+    fputs(expression, file);
+
+    fclose(file);
+
+    calculate_expression("D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_3_test_2.txt");
+
+    file = fopen(filename, "r");
+
+    char data[100] = "";
+    fgets(data, sizeof(data), file);
+
+    fclose(file);
+
+    char res[] = "(2 * 3) + 3 = 19.00 ";
+
+    assert(strcmp(data, res));
+}
+
+void test_calculate() {
+    test_calculate_expression_2_operand();
+    test_calculate_expression_3_operand();
+}
+
 int main(){
 
     test_matrix_transpose();
     test_convert_float();
+    test_calculate();
 }
