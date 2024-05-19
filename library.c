9ee936142b2801b13305cc5ec07252c5b0df9f65
Gger206 <arteml22012006@gmail.com>
2024-05-19 23:57:18 +0300

    task 5

--- library.c
+++ library.c
@@ -560,7 +560,7 @@ void filter_word(const char* filename, char* source_word) {
 
 
 void test_filter_word_1_empty_file() {
-    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_4_test_1.txt";
+    const char filename[] = "D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_4_test_1.txt";
 
     generate_string(filename, "");
     char source_word[] = "word";
@@ -576,7 +576,7 @@ void test_filter_word_1_empty_file() {
 
 
 void test_filter_word_2_sequence_not_in() {
-    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_4_test_2.txt";
+    const char filename[] = "D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_4_test_2.txt";
 
     generate_string(filename, "qwe rtg ngfm");
     char source_word[] = "bgmk";
@@ -591,7 +591,7 @@ void test_filter_word_2_sequence_not_in() {
 }
 
 void test_filter_word_3_sequence_in() {
-    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_4_test_3.txt";
+    const char filename[] = "D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_4_test_3.txt";
 
     generate_string(filename, "bvgh vhv jaef kfk");
     char source_word[] = "v";
@@ -611,10 +611,208 @@ void test_filter_word() {
     test_filter_word_3_sequence_in();
 }
 
+void generate_text_file(const char* filename, int lines, int word, int max_word_size) {
+
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < lines; i++) {
+        for (int j = 0; j < word; j++) {
+            for (int k = 0; k < rand() % max_word_size + 1; k++) {
+                fprintf(file, "%c", 'a' + rand() % 26);
+            }
+            fprintf(file, " ");
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
+
+void leave_longest_word(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+
+    char buff[MAX_STRING_SIZE] = "";
+    char* rec_ptr = _stringBuffer;
+
+    fgets(buff, sizeof(buff), file);
+
+    size_t length = strlen_(buff);
+    length = length == 0 ? 1 : length;
+
+    rec_ptr = copy(buff, buff + length - 1, rec_ptr);
+    *rec_ptr++ = ' ';
+
+
+    size_t amount_word_in_line = 0;
+    char* begin_search = _stringBuffer;
+    while (getWord(begin_search, &_bag.words[_bag.size])) {
+        begin_search = _bag.words[_bag.size].end + 1;
+        amount_word_in_line++;
+        _bag.size++;
+    }
+
+
+    while (fgets(buff, sizeof(buff), file)) {
+        rec_ptr = copy(buff, buff + strlen_(buff) - 1, rec_ptr);
+        *rec_ptr++ = ' ';
+
+        while (getWord(begin_search, &_bag.words[_bag.size])) {
+            begin_search = _bag.words[_bag.size].end + 1;
+            _bag.size++;
+        }
+    }
+
+    fclose(file);
+
+
+    file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    for (size_t i = 0; i < _bag.size; i += amount_word_in_line) {
+        WordDescriptor word_max_length = _bag.words[i];
+        size_t max_length = word_max_length.end - word_max_length.begin + 1;
+
+        for (size_t j = i + 1; j < i + amount_word_in_line; j++) {
+            size_t current_length = _bag.words[j].end - _bag.words[j].begin + 1;
+
+            if (current_length > max_length) {
+                word_max_length = _bag.words[j];
+                max_length = current_length;
+            }
+        }
+
+        char* write_ptr = word_max_length.begin;
+        while (write_ptr <= word_max_length.end) {
+            fprintf(file, "%c", *write_ptr);
+            write_ptr++;
+        }
+
+        fprintf(file, "\n");
+    }
+
+    free_bag(&_bag);
+    fclose(file);
+}
+
+
+void test_leave_longest_word_1_empty_file() {
+    const char filename[] = "D:\\BSTU Shukhov\\LionProjects\\lab_19_19\task_5_test_1.txt";
+
+    FILE* file = fopen(filename, "w");
+    fclose(file);
+
+    leave_longest_word(filename);
+
+    file = fopen(filename, "r");
+
+    char data[100] = "";
+    fprintf(file, "%s", data);
+
+    fclose(file);
+
+    assert(strcmp(data, "") == 0);
+}
+
+
+void test_leave_longest_word_2_one_element_in_line() {
+    const char filename[] = "D:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_5_test_2.txt";
+
+    char line1[] = "rty";
+    char line2[] = "jkl";
+
+    FILE* file = fopen(filename, "w");
+
+    fprintf(file, "%s \n", line1);
+    fprintf(file, "%s \n", line2);
+
+    fclose(file);
+
+    leave_longest_word(filename);
+
+    file = fopen(filename, "r");
+
+    char res_line1[4] = "";
+    fscanf(file, "%s\n", res_line1);
+
+    char res_line2[10] = "";
+    fscanf(file, "%s\n", res_line2);
+
+    char res_line3[4] = "";
+    fscanf(file, "%s\n", res_line3);
+
+    fclose(file);
+
+    assert(strcmp(line1, res_line1) == 0);
+    assert(strcmp(line2, res_line2) == 0);
+}
+
+
+void test_leave_longest_word_3_more_element_in_line() {
+    const char filename[] = "DD:\\BSTU Shukhov\\LionProjects\\lab_19_19\\task_5_test_3.txt";
+
+    char line1[] = "asdfghjkl rtg ";
+    char line2[] = "ikmu qqe ";
+    char line3[] = "cvb tyu ";
+
+    FILE* file = fopen(filename, "w");
+
+    fputs(line1, file);
+    fprintf(file, "\n");
+    fputs(line2, file);
+    fprintf(file, "\n");
+    fputs(line3, file);
+    fprintf(file, "\n");
+
+    fclose(file);
+
+    leave_longest_word(filename);
+
+    file = fopen(filename, "r");
+
+    char res_line1[10] = "";
+    fscanf(file, "%s\n", res_line1);
+
+    char res_line2[10] = "";
+    fscanf(file, "%s\n", res_line2);
+
+    char res_line3[10] = "";
+    fscanf(file, "%s\n", res_line3);
+
+    fclose(file);
+
+    char res1[] = "asdfghjkl";
+    char res2[] = "ikmu";
+    char res3[] = "cvb";
+
+    assert(strcmp(res1, res_line1) == 0);
+    assert(strcmp(res2, res_line2) == 0);
+    assert(strcmp(res3, res_line3) == 0);
+}
+
+
+void test_leave_longest() {
+    test_leave_longest_word_1_empty_file();
+    test_leave_longest_word_2_one_element_in_line();
+    test_leave_longest_word_3_more_element_in_line();
+}
+
 int main(){
 
     test_matrix_transpose();
     test_convert_float();
     test_calculate();
     test_filter_word();
+    test_leave_longest();
 }
